29e679a00d9026171ba6573122af2db71797ddca
Rajhans Kumar <rajhans@webmobriltechnologies.com>
2025-05-03 13:18:18 +0530

    updated code

--- utility/uploadAWS.js
+++ utility/uploadAWS.js
@@ -1,10 +1,16 @@
 const path = require("path");
 const multer = require("multer");
-const aws = require("aws-sdk");
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3'); 
 const multerS3 = require("multer-s3");
 
-
-const s3 = new aws.S3();
+// Initialize the AWS S3 client
+const s3Client = new S3Client({
+  region: 'us-east-1', // Specify your AWS region here
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
+});
 
 // Create the uploadFile function
 const uploadFile = (fieldName) => {
@@ -13,7 +19,7 @@ const uploadFile = (fieldName) => {
   const bucketName = "fetch-delivery"; // Make sure this is the correct bucket name
 
   const fileStorage = multerS3({
-    s3,
+    s3: s3Client, // Using the new S3 Client (v3)
     bucket: bucketName,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     metadata: (req, file, cb) => {
@@ -23,14 +29,11 @@ const uploadFile = (fieldName) => {
       });
     },
     key: (req, file, cb) => {
-      const uploadedFileName = `${file.fieldname}-${Date.now()}${path.extname(
-        file.originalname
-      )}`;
+      const uploadedFileName = `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`;
       console.log("Generated File Name:", uploadedFileName); // Debugging log
       cb(null, uploadedFileName);
     },
   });
-  
 
   return multer({ storage: fileStorage }).fields([
     { name: 'mainImage', maxCount: 1 },
